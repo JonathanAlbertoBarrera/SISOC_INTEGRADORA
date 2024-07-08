9260279f0368a3ca733d937317ab8f7ecd899103
JonathanAlbertoBarrera <20233tn110@utez.edu.mx>
2024-07-07 22:37:17 -0600

    Funcion getAll dao Usuario

--- src/main/java/mx/edu/utez/practica3e/dao/UsuarioDao.java
+++ src/main/java/mx/edu/utez/practica3e/dao/UsuarioDao.java
@@ -7,12 +7,13 @@ import mx.edu.utez.practica3e.model.Usuario;
 import mx.edu.utez.practica3e.utils.DatabaseConnectionManager;
 
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 // Estas clases DAO permiten el uso de funciones CRUD
 public class UsuarioDao {
 
-    //Programar una función R (lectura) para obtener un usuario
-    //con el fin de hacer el inicio de sesión
+    //Obtener un solo Usuario, considerando que hay datos tambien en la tabla Persona y Rol
     public Usuario getOne(String correo, String contrasena) {
         Usuario usuario = null;
         String query = "SELECT u.id_usuario, u.correo, u.contrasena, u.estatus, " +
@@ -55,6 +56,49 @@ public class UsuarioDao {
         }
 
         return usuario;
+    }
+
+    //Se obtienen todos los usuarios
+    public List<Usuario> getAll() {
+        List<Usuario> usuarios = new ArrayList<>();
+        String query = "SELECT u.id_usuario, u.correo, u.contrasena, u.estatus, " +
+                "p.id_persona, p.nombre, p.apellidos, p.telefono, p.sexo, " +
+                "r.id, r.tipoRol " +
+                "FROM usuario u " +
+                "JOIN persona p ON u.id_persona = p.id_persona " +
+                "JOIN roles r ON u.id_rol = r.id";
+
+        try (Connection con = DatabaseConnectionManager.getConnection();
+             PreparedStatement ps = con.prepareStatement(query);
+             ResultSet rs = ps.executeQuery()) {
+
+            while (rs.next()) {
+                Persona persona = new Persona();
+                persona.setIdPersona(rs.getInt("id_persona"));
+                persona.setNombre(rs.getString("nombre"));
+                persona.setApellidos(rs.getString("apellidos"));
+                persona.setTelefono(rs.getString("telefono"));
+                persona.setSexo(rs.getString("sexo"));
+
+                Rol rol = new Rol();
+                rol.setId(rs.getInt("id"));
+                rol.setTipoRol(rs.getString("tipoRol"));
+
+                Usuario usuario = new Usuario();
+                usuario.setIdUsuario(rs.getInt("id_usuario"));
+                usuario.setCorreo(rs.getString("correo"));
+                usuario.setContrasena(rs.getString("contrasena"));
+                usuario.setEstatus(rs.getBoolean("estatus"));
+                usuario.setPersona(persona);
+                usuario.setRol(rol);
+
+                usuarios.add(usuario);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return usuarios;
     }
 
     //Se inserta tanto en la tabla usuario como en persona
--- src/main/java/mx/edu/utez/practica3e/utils/DatabaseConnectionManager.java
+++ src/main/java/mx/edu/utez/practica3e/utils/DatabaseConnectionManager.java
@@ -6,7 +6,7 @@ import java.sql.Connection;
 import java.sql.SQLException;
 
 public class DatabaseConnectionManager {
-    private static final String JDBC_URL = "jdbc:mysql://localhost:3306/sisoc";
+    private static final String JDBC_URL = "jdbc:mysql://localhost:3306/sisocBD";
     private static final String USERNAME = "root";
     private static final String PASSWORD = "";
     private static final HikariConfig config = new HikariConfig();
